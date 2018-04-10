a6cefa8dd080f826b9e218fdc5ab0640b2a91e95
KevinJan18 <kevin@vjkd.nl>
2018-04-10 20:33:57 +0200

    Test

--- main.h
+++ main.h
@@ -8,13 +8,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#if defined(_WIN32)
+#define PORT 8000
+#include <winsock2.h>
+#elif defined(__linux__)
+#define PORT 9000
+#include <sys/socket.h>
+#endif
 int main(int argc, const char* argv[]){
-    while(true){
-        printf("Hello Worlda!");
-        sleep(10000);
-    }
-    exit(0);
+    printf(PORT + "");
+    //int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    printf("Hello World");
 }
 
 #endif //CBACKEND_MAIN_H
