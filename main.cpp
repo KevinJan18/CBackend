96f32d4081a1c322a7712937b396093f14c93e64
KevinJan18 <kevin@vjkd.nl>
2018-04-10 21:19:43 +0200

    Port 80

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main(int argc, const char* argv[]) {
 
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htons(INADDR_ANY);
-    servaddr.sin_port = htons(22000);
+    servaddr.sin_port = htons(80);
 
     bind(listen_fd, (struct sockaddr *) &servaddr, sizeof(servaddr));
 
