e67ba19527c8b0befef050db4a2fbe4ad778da51
KevinJan18 <kevin@vjkd.nl>
2018-04-10 19:02:00 +0200

    Small Thingy

--- main.h
+++ main.h
@@ -6,9 +6,13 @@
 #define CBACKEND_MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
 
 int main(int argc, const char* argv[]){
     printf("Hello World!");
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    exit(0);
 }
 
 #endif //CBACKEND_MAIN_H
