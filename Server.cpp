ea9fe9a1084f73a3bf4c43e8da9b174ac98e7322
KevinJan18 <kevin@vjkd.nl>
2018-04-12 19:57:13 +0200

    Changed Port

--- Server.cpp
+++ Server.cpp
@@ -5,20 +5,20 @@
 #include "Server.h"
 
 
-Server::Server(int port, char* ip_addr){
+Server::Server(int port, char *ip_addr) {
     _port = port;
     //Saferead the IP
     _ip_addr = (char *) malloc(sizeof(char) * 15);
     cpString(ip_addr, _ip_addr);
 }
 
-void Server::startup(){
+void Server::startup() {
 
     _listen_fd = (int) socket(AF_INET, SOCK_STREAM, 0);
 
     _servaddr.sin_family = AF_INET;
     _servaddr.sin_addr.s_addr = htons(INADDR_ANY);
-    _servaddr.sin_port = htons(80);
+    _servaddr.sin_port = htons(59);
 
     bind((SOCKET) _listen_fd, (struct sockaddr *) &_servaddr, sizeof(_servaddr));
 
@@ -27,38 +27,43 @@ void Server::startup(){
     _comm_fd = (int) accept((SOCKET) _listen_fd, (struct sockaddr *) NULL, NULL);
 }
 
-void Server::getData(){
-    while (1) {
+void Server::getData() {
+    zeroTarget(_data);
+    printf("Zeroed Data\n");
+    printf("Listening\n");
+    while(*_data == 0) {
         read(_comm_fd, _data, 100);
-
-        printf("Echoing back - %s\n",_data);
-
-        write(_comm_fd, _data, (unsigned int) (strlen(_data) + 1));
     }
+    printf("Got Data\n");
+    printf("Echoing back - %s\n", _data);
+    write(_comm_fd, _data, (unsigned int) (strlen(_data)));
+    printf("Wrote Data\n");
 
 }
 
 char *Server::toString() {
-    char* string = (char*) malloc(sizeof(_ip_addr));
+    char *string = (char *) malloc(sizeof(_ip_addr));
     cpString(_ip_addr, string);
     return string;
 }
 
-void Server::cpString(char* source, char* dest){
-    char* desttemp = dest;
-    char* sourcetemp = source;
-    while(*sourcetemp){
-        *dest = *sourcetemp;
+//Copies a byte array from one place to another, used for strings
+void Server::cpString(char *source, char *dest) {
+    zeroTarget(dest);
+    int size = sizeof(source) + 1;
+    for (int i = 0; i <= size; i++) {
+        *dest = *source;
         dest++;
-        sourcetemp++;
+        source++;
     }
-    source = sourcetemp;
-    dest = desttemp;
+    source -= size;
+    dest -= size;
 }
 
-void Server::zeroTarget(char* target){
+//Zeroes the targeted byte array.
+void Server::zeroTarget(char *target) {
     int size = sizeof(target);
-    for(int i = 0; i <= size;i++){
+    for (int i = 0; i <= size; i++) {
         *target = 0;
         target++;
     }
--- main.cpp
+++ main.cpp
@@ -8,7 +8,10 @@
 int main(int argc, const char* argv[]) {
     char* ip_addr = (char *) "127.0.0.1";
     server = Server(80, (char *) "127.0.0.2");
-    char* string = server.toString();
+    server.startup();
+    printf("Started Up\n");
+    server.getData();
+
     return 0;
 }
 
